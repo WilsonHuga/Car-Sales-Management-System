d86f94c7d1f16c42e74cfb909622594217e39313
270492384 <270492384@CIT3-205-12>
2025-05-28 15:04:39 +1200

    Add some changes

---
+++ Car Sales Management System/DataModels/User.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Car_Sales_Management_System.DataModels
+{
+    public enum UserRole
+    {
+        Admin,
+        Salesperson
+    }
+
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("username")]
+        public string Username { get; set; } = string.Empty;
+
+        [BsonElement("passwordHash")]
+        public string PasswordHash { get; set; } = string.Empty;
+
+        [BsonElement("role")]
+        public string Role { get; set; } = string.Empty;
+    }
+}
